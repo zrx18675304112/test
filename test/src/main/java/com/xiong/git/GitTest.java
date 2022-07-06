03f4960b23c59a07d72400c66892e9ed40a40f66
zrx <1279632679@qq.com>
2022-07-06 09:44:48 +0800

    first commit

--- test/src/main/java/com/xiong/git/GitTest.java
+++ test/src/main/java/com/xiong/git/GitTest.java
@@ -2,6 +2,6 @@ package com.xiong.git;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("hello github");
+        System.out.println("hello github2");
     }
 }

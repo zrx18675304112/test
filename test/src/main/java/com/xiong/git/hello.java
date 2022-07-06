35521c9305d22804b66db2c0d41bac658f35fca4
zrx <1279632679@qq.com>
2022-07-06 10:44:33 +0800

    master commit4

--- test/src/main/java/com/xiong/git/hello.java
+++ test/src/main/java/com/xiong/git/hello.java
@@ -4,5 +4,6 @@ public class hello {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("hello1");
+        System.out.println("hello2");
     }
 }

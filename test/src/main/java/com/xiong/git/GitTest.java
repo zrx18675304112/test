a47741951165c356c0df43dda5e4551ae8dcdc23
zrx <1279632679@qq.com>
2022-07-06 09:54:08 +0800

    hot first commit

--- test/src/main/java/com/xiong/git/GitTest.java
+++ test/src/main/java/com/xiong/git/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello github2");
         System.out.println("hello github3");
+        System.out.println("hello github4");
     }
 }
